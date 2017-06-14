da923beb2f44c39fc79590b1b64b4d388b384a8d
Johan Alvarez <llstarscreamll@hotmail.com>
2017-06-14 01:17:12 -0500

    remove unsed imports

--- admin-lte.module.ts
+++ admin-lte.module.ts
@@ -1,9 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-
-import { TranslateModule } from '@ngx-translate/core';
-import { Ng2BootstrapModule } from 'ngx-bootstrap';
 
 import { CoreSharedModule } from './../core/core.shared.module';
 
